a2ff6d1f2adf505014356bc53a78d341f0213ee4
Carlijn Assen <c.m.a.assen@student.rug.nl>
2019-04-28 10:46:21 +0200

    modified assignment 2, bijna af, alleen 2d  nog

--- assignment2.py
+++ assignment2.py
@@ -72,32 +72,39 @@ for sent in sents:
 
 print("*Assignment 2(c)*\n")
 
-print("Top 20 character level unigrams(ordered by descending frequency:\n")
+print("\n\nTop 20 character level unigrams(ordered by descending frequency:\n")
 print(sorted(ch_types_list)[0:21])
 
-print("Top 20 character level bigrams(ordered by descending frequency:\n")
-bigrams = nltk.bigrams(nltk.word_tokenize(sent))
+print("\n\nTop 20 character level bigrams(ordered by descending frequency:\n")
+ch_tokens_list = []
+for i in range(len(rawText)):
+    ch_tokens_list.append(rawText[i])
+
+bigrams = nltk.bigrams(ch_tokens_list)
+bigrams_list = []
+
 fdist = nltk.FreqDist(bigrams)
-n = 0
-for i in sorted(fdist.keys()):
-    print(i, fdist[i])
-    n += 1
-    if n == 20:
-        break
-    else:
-        continue
+for b,f in fdist.items():
+	bigrams_list.append((b,f))
 
+bigrams_list = sorted(bigrams_list, key=lambda tup: tup[1], reverse = True)
 
+for b, f in bigrams_list[:21]:
+	print(b, f)
 
-# is het nu descending???????????? dit klopt niet helemaal
+print("\n\nTop 20 character level trigrams(ordered by descending frequency:\n")
 
-""">>> sent = ['The', 'cat', 'that', 'sat', 'on', 'the', 'mat',2...    'also', 'sat', 'on', 'the', 'sofa']
-3>>>list(nltk.bigrams(sent))4[('The', 'cat'), ('cat', 'that'), ('that', 'sat'), ('sat', 'on'),5...    ('on', 'the'), ('the', 'mat'), ('mat', 'also'), ('also',6...    'sat'), ('sat', 'on'), ('on', 'the'), ('the', 'sofa')]
+trigrams = nltk.trigrams(ch_tokens_list)
+trigrams_list = []
 
-Words and TokenisationBigrams’ countsLet’s get frequencies...1>>> sent = ['The', 'cat', 'that', 'sat', 'on', 'the', 'mat',2...    'also', 'sat', 'on', 'the', 'sofa']3>>> bigrams = nltk.bigrams(sent)4>>> fdist = nltk.FreqDist(bigrams)5>>>forb,finfdist.items():6...print(b,f)7...8('mat', 'also') 19('The', 'cat') 110('on', 'the') 211('the', 'sofa') 112('sat', 'on') 213('cat', 'that') 114('that', 'sat') 115('also', 'sat') 116('the', 'mat') 
+fdist = nltk.FreqDist(trigrams)
+for t,f in fdist.items():
+	trigrams_list.append((t,f))
 
-c)  the top 20character-level unigrams, bigrams, and trigrams, ordered by descend-ing frequency (from the most frequent to the least).  Please, add a comment speci-fying what you think this information could be useful for.(d)  the top 20word-level unigrams, bigrams and trigrams, ordered by descending fre-quency.  Please, add a comment specifying what you think this information couldbe useful for."""
+trigrams_list = sorted(trigrams_list, key=lambda tup: tup[1], reverse = True)
 
+for t, f in trigrams_list[:21]:
+	print(t, f)
 
 
 
